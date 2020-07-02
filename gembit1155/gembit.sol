00181286c7ba6dde1040644bf1cd3355f47c5cfe
Kornrapat Pongmala <kornrapatp@berkeley.edu>
2020-07-02 12:05:32 +0700

    added 1155

--- gembit1155/gembit.sol
+++ gembit1155/gembit.sol
@@ -6,6 +6,37 @@ import "../interfaces/ierc1155receiver.sol";
 
 
 contract gembit is ERC1155, ERC165 {
+    mapping (uint256 => mapping(address => uint256)) internal balances;
+    mapping (address => mapping(address => bool)) internal operatorApproval;
+    address minter;
+
+    bytes4 constant private INTERFACE_SIGNATURE_ERC165 = 0x01ffc9a7;
+
+    /*
+        bytes4(keccak256("safeTransferFrom(address,address,uint256,uint256,bytes)")) ^
+        bytes4(keccak256("safeBatchTransferFrom(address,address,uint256[],uint256[],bytes)")) ^
+        bytes4(keccak256("balanceOf(address,uint256)")) ^
+        bytes4(keccak256("balanceOfBatch(address[],uint256[])")) ^
+        bytes4(keccak256("setApprovalForAll(address,bool)")) ^
+        bytes4(keccak256("isApprovedForAll(address,address)"));
+    */
+    bytes4 constant private INTERFACE_SIGNATURE_ERC1155 = 0xd9b67a26;
+
+    function supportsInterface(bytes4 _interfaceId)
+    public
+    view
+    returns (bool) {
+         if (_interfaceId == INTERFACE_SIGNATURE_ERC165 ||
+             _interfaceId == INTERFACE_SIGNATURE_ERC1155) {
+            return true;
+         }
+
+         return false;
+    }
+
+    constructor() public {
+        minter = msg.sender;
+    }
     /**
         @dev Either `TransferSingle` or `TransferBatch` MUST emit when tokens are transferred, including zero value transfers as well as minting or burning (see "Safe Transfer Rules" section of the standard).
         The `_operator` argument MUST be the address of an account/contract that is approved to make the transfer (SHOULD be msg.sender).
@@ -42,6 +73,14 @@ contract gembit is ERC1155, ERC165 {
     */
     event URI(string _value, uint256 indexed _id);
 
+    function mint(address _to, uint256 _id, uint256 _value) external {
+        require(msg.sender == minter, "You are not the minter!");
+        require(_to != address(0), "Cannot mint to address 0!");
+        balances[_id][_to] += _value;
+        
+        emit TransferSingle(msg.sender, address(0), _to, _id, _value);
+    }
+
     /**
         @notice Transfers `_value` amount of an `_id` from the `_from` address to the `_to` address specified (with safety call).
         @dev Caller must be approved to manage the tokens being transferred out of the `_from` account (see "Approval" section of the standard).
@@ -56,7 +95,21 @@ contract gembit is ERC1155, ERC165 {
         @param _value   Transfer amount
         @param _data    Additional data with no specified format, MUST be sent unaltered in call to `onERC1155Received` on `_to`
     */
-    function safeTransferFrom(address _from, address _to, uint256 _id, uint256 _value, bytes calldata _data) external;
+    function safeTransferFrom(address _from, address _to, uint256 _id, uint256 _value, bytes calldata _data) external override {
+        require(msg.sender == _from || operatorApproval[_from][msg.sender], "Not authorized!");
+        require(_to != address(0), "Cannot transfer to 0 address.");
+        require(balances[_id][_from] >= _value, "Insufficient Fund.");
+        balances[_id][_from] -= _value;
+        balances[_id][_to] += _value;
+        
+        emit TransferSingle(msg.sender, _from, _to, _id, _value);
+
+        // Now that the balance is updated and the event was emitted,
+        // call onERC1155Received if the destination is a contract.
+        if (_to.isContract()) {
+            _doSafeTransferAcceptanceCheck(msg.sender, _from, _to, _id, _value, _data);
+        }
+    }
 
     /**
         @notice Transfers `_values` amount(s) of `_ids` from the `_from` address to the `_to` address specified (with safety call).
@@ -74,7 +127,26 @@ contract gembit is ERC1155, ERC165 {
         @param _values  Transfer amounts per token type (order and length must match _ids array)
         @param _data    Additional data with no specified format, MUST be sent unaltered in call to the `ERC1155TokenReceiver` hook(s) on `_to`
     */
-    function safeBatchTransferFrom(address _from, address _to, uint256[] calldata _ids, uint256[] calldata _values, bytes calldata _data) external;
+    function safeBatchTransferFrom(address _from, address _to, uint256[] calldata _ids, uint256[] calldata _values, bytes calldata _data) external override {
+        require(msg.sender == _from || operatorApproval[_from][msg.sender], "Not authorized!");
+        require(_to != address(0), "Cannot transfer to 0 address.");
+        require(_ids.length == _values.length, "_ids and _values array length must match.");
+        for (uint256 i = 0; i < _ids.length; ++i) {
+            uint256 id = _ids[i];
+            uint256 value = _values[i];
+            require(balances[id][_from] >= value, "Insufficient Fund.");
+            balances[id][_from] -= value;
+            balances[id][_to] += value;
+        }
+        // MUST emit event
+        emit TransferBatch(msg.sender, _from, _to, _ids, _values);
+
+        // Now that the balances are updated and the events are emitted,
+        // call onERC1155BatchReceived if the destination is a contract.
+        if (_to.isContract()) {
+            _doSafeBatchTransferAcceptanceCheck(msg.sender, _from, _to, _ids, _values, _data);
+        }
+    }
 
     /**
         @notice Get the balance of an account's tokens.
@@ -82,7 +154,9 @@ contract gembit is ERC1155, ERC165 {
         @param _id     ID of the token
         @return        The _owner's balance of the token type requested
      */
-    function balanceOf(address _owner, uint256 _id) external view returns (uint256);
+    function balanceOf(address _owner, uint256 _id) external view override returns (uint256) {
+        return balances[_id][_owner];
+    }
 
     /**
         @notice Get the balance of multiple account/token pairs
@@ -90,7 +164,17 @@ contract gembit is ERC1155, ERC165 {
         @param _ids    ID of the tokens
         @return        The _owner's balance of the token types requested (i.e. balance for each (owner, id) pair)
      */
-    function balanceOfBatch(address[] calldata _owners, uint256[] calldata _ids) external view returns (uint256[] memory);
+    function balanceOfBatch(address[] calldata _owners, uint256[] calldata _ids) external view override returns (uint256[] memory) {
+        require(_owners.length == _ids.length, "The given arrays did not match.");
+
+        uint256[] memory balances_ = new uint256[](_owners.length);
+
+        for (uint256 i = 0; i < _owners.length; ++i) {
+            balances_[i] = balances[_ids[i]][_owners[i]];
+        }
+
+        return balances_;
+    }
 
     /**
         @notice Enable or disable approval for a third party ("operator") to manage all of the caller's tokens.
@@ -98,7 +182,10 @@ contract gembit is ERC1155, ERC165 {
         @param _operator  Address to add to the set of authorized operators
         @param _approved  True if the operator is approved, false to revoke approval
     */
-    function setApprovalForAll(address _operator, bool _approved) external;
+    function setApprovalForAll(address _operator, bool _approved) external override {
+        operatorApproval[msg.sender][_operator] = _approved;
+        emit ApprovalForAll(msg.sender, _operator, _approved);
+    }
 
     /**
         @notice Queries the approval status of an operator for a given owner.
@@ -106,5 +193,28 @@ contract gembit is ERC1155, ERC165 {
         @param _operator  Address of authorized operator
         @return           True if the operator is approved, false if not
     */
-    function isApprovedForAll(address _owner, address _operator) external view returns (bool);
+    function isApprovedForAll(address _owner, address _operator) external view override returns (bool) {
+        return operatorApproval[_owner][_operator];
+    }
+
+    function _doSafeTransferAcceptanceCheck(address _operator, address _from, address _to, uint256 _id, uint256 _value, bytes memory _data) internal {
+
+        // If this was a hybrid standards solution you would have to check ERC165(_to).supportsInterface(0x4e2312e0) here but as this is a pure implementation of an ERC-1155 token set as recommended by
+        // the standard, it is not necessary. The below should revert in all failure cases i.e. _to isn't a receiver, or it is and either returns an unknown value or it reverts in the call to indicate non-acceptance.
+
+
+        // Note: if the below reverts in the onERC1155Received function of the _to address you will have an undefined revert reason returned rather than the one in the require test.
+        // If you want predictable revert reasons consider using low level _to.call() style instead so the revert does not bubble up and you can revert yourself on the ERC1155_ACCEPTED test.
+        require(ERC1155TokenReceiver(_to).onERC1155Received(_operator, _from, _id, _value, _data) == ERC1155_ACCEPTED, "contract returned an unknown value from onERC1155Received");
+    }
+
+    function _doSafeBatchTransferAcceptanceCheck(address _operator, address _from, address _to, uint256[] memory _ids, uint256[] memory _values, bytes memory _data) internal {
+
+        // If this was a hybrid standards solution you would have to check ERC165(_to).supportsInterface(0x4e2312e0) here but as this is a pure implementation of an ERC-1155 token set as recommended by
+        // the standard, it is not necessary. The below should revert in all failure cases i.e. _to isn't a receiver, or it is and either returns an unknown value or it reverts in the call to indicate non-acceptance.
+
+        // Note: if the below reverts in the onERC1155BatchReceived function of the _to address you will have an undefined revert reason returned rather than the one in the require test.
+        // If you want predictable revert reasons consider using low level _to.call() style instead so the revert does not bubble up and you can revert yourself on the ERC1155_BATCH_ACCEPTED test.
+        require(ERC1155TokenReceiver(_to).onERC1155BatchReceived(_operator, _from, _ids, _values, _data) == ERC1155_BATCH_ACCEPTED, "contract returned an unknown value from onERC1155BatchReceived");
+    }
 }
